0734b867dbcd4eb97df2bc96db94fad11d911367
Karan Songara <karansongara290@gmail.com>
2025-01-10 21:14:55 +0530

    Product Detail Project

---
+++ ProductDetail/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductPractice.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+        public IActionResult AddList()
+        {
+            return View();
+        }
+    }
+}
